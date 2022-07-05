2a943cc3ff02a6af8ba4e0cd6099b79d16f37d07
EC2 Default User <ec2-user@ip-172-31-72-137.ec2.internal>
2022-07-05 00:03:22 +0000

    doing aws tutorial stuff

---
+++ variables.py
@@ -0,0 +1,22 @@
+a_str = "i am a string in quotes check me out"
+print(a_str)
+
+my_float = 6.6
+b_str = "<that is a float check it out"
+print(my_float, b_str)
+
+shopping_list = ["meat", "potatos", "pasta"]
+print(shopping_list)
+
+a_dict = {"userId": "JBloggs"} # dictionary is abbreviated to dict
+print(a_dict)
+
+my_var = another_variable # variable is abbreviated to var
+
+test_function = my_function() #function is abbreviated to func
+
+example_tuple = ("apple", "orange", "pear")
+print(example_tuple)
+
+boolean_values = True # boolean is abbreviated to bool
+print(boolean_values)
